01c21130ca71006e3761f00ae5895da2a5c3edc9
LOKESH KRISHNAA <75234646+curiouzs@users.noreply.github.com>
2022-05-21 22:55:43 +0530

    Create erose_dilate..py

---
+++ erose_dilate..py
@@ -0,0 +1,23 @@
+# Import the necessary packages
+import numpy as np
+import cv2
+import matplotlib.pyplot as plt
+# Load the Image
+img1=np.zeros((100,500),dtype='uint8')
+font=cv2.FONT_HERSHEY_COMPLEX_SMALL
+
+# Create the Text using cv2.putText
+cv2.putText(img1,'LOKESH KRISHNAA',(5,70),font,2,(255),5,cv2.LINE_AA)
+plt.imshow(img1,cmap='gray')
+
+# Create the structuring element
+kernel1=cv2.getStructuringElement(cv2.MORPH_CROSS,(7,7))
+
+# Erode the image
+img_erode=cv2.erode(img1,kernel1)
+plt.imshow(img_erode,cmap='gray')
+
+# Dilate the image
+img_dilate=cv2.dilate(img1,kernel1)
+plt.imshow(img_dilate,cmap='gray')
+
